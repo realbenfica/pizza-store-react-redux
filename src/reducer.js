bc691ef58d9228c551e0687079f7edd830e64a13
“ALEX <alexgtr84@gmail.com>
2019-01-24 14:17:57 +0100

    Setup

---
+++ src/reducer.js
@@ -0,0 +1,45 @@
+const initialState = [
+    {
+      id: 1,
+      name: 'Pizza Margarita',
+      description: 'Minimalism is key!',
+      ingredients: [
+        'tomato',
+        'mozarella',
+        'basil'
+      ]
+    },
+    {
+      id: 2,
+      name: 'Pizza Napoletana',
+      description: 'The mafia knows whats best for you',
+      ingredients: [
+        'tomato',
+        'mozarella'
+      ]
+    },
+    {
+      id: 3,
+      name: 'Pizza Bianca',
+      description: 'Did somebody say oil?',
+      ingredients: [
+        'olive oil',
+        'salt',
+        'rosemary'
+      ]
+    }
+  ]
+
+const reducer = (initialState = [], action = {}) => {
+    switch (action.type) {
+    default:
+      return initialState
+    }
+  }
+  
+  export default reducer
+
+
+
+
+  
